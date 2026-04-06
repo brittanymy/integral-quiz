4f9dcb2a946451ddd72aa7a2dec1c6c8dfe40190
brittanymy <brittany.young2001@gmail.com>
2026-04-05 21:09:38 -0400

    added mathjax, updated logo and site name, added quiz questions array structure

---
+++ assets/js/index.js
@@ -0,0 +1,144 @@
+const quizQuestions = [
+    {
+        category: 'Multi-Term Expressions',
+        question: 
+        choices: []
+        answer:
+        feedback: []
+        solution:
+    },  
+    {
+        category: 'Multi-Term Expressions',
+        question:
+        choices: []
+        answer:
+        feedback: []
+        solution:
+    },
+    {
+        category: 'Multi-Term Expressions',
+        question:
+        choices: []
+        answer:
+        feedback: []
+        solution:
+    },
+    {
+        category: 'Multi-Term Expressions',
+        question:
+        choices: []
+        answer:
+        feedback: []
+        solution:
+    },
+    {
+        category: 'Multi-Term Expressions',
+        question:
+        choices: []
+        answer:
+        feedback: []
+        solution:
+    },    
+    {
+        category: 'U-Substitution',
+        question:
+        choices: []
+        answer:
+        feedback: []
+        solution:
+    },    
+    {
+        category: 'U-Substitution',
+        question:
+        choices: []
+        answer:
+        feedback: []
+    },    {
+        category: 'U-Substitution',
+        question:
+        choices: []
+        answer:
+        feedback: []
+        solution:
+    },    {
+        category: 'U-Substitution',
+        question:
+        choices: []
+        answer:
+        feedback: []
+        solution:
+    },    {
+        category: 'U-Substitution',
+        question:
+        choices: []
+        answer:
+        feedback: []
+    },    {
+        category: 'Decision-Based Problems',
+        question:
+        choices: []
+        answer:
+        feedback: []
+    },    {
+        category: 'Decision-Based Problems',
+        question:
+        choices: []
+        answer:
+        feedback: []
+        solution:
+    },    {
+        category: 'Decision-Based Problems',
+        question:
+        choices: []
+        answer:
+        feedback: []
+        solution:
+    },    {
+        category: 'Decision-Based Problems',
+        question:
+        choices: []
+        answer:
+        feedback: []
+    },    {
+        category: 'Decision-Based Problems',
+        question:
+        choices: []
+        answer:
+        feedback: []
+        solution:
+    },    {
+        category: 'Application Problems',
+        question:
+        choices: []
+        answer:
+        feedback: []
+    },    {
+        category: 'Application Problems',
+        question:
+        choices: []
+        answer:
+        feedback: []
+        solution:
+    },    {
+        category: 'Application Problems',
+        question:
+        choices: []
+        answer:
+        feedback: []
+        solution: 'Application Problems',
+    },    {
+        category:
+        question:
+        choices: []
+        answer:
+        feedback: []
+        solution:
+    },    {
+        category: 'Application Problems',
+        question:
+        choices: []
+        answer:
+        feedback: []
+        solution:
+    },
+]
